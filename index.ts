4188c5cb987ee0238c8b17ab5fba7e3dbd350ab9
ywatanabe <yu.w.tennis@gmail.com>
2025-04-05 01:06:54 +0900

    exp

--- index.ts
+++ index.ts
@@ -6,24 +6,24 @@ async function main() {
     const orgName: string = process.env.PULUMI_ORG_NAME!;
     const envName: string = process.env.PULUMI_ENV_NAME!;
     const projName: string = pulumi.getProject();
-    const client = esc.DefaultClient();
+    // const client = esc.DefaultClient();
 
-    const openEnv = await client.openAndReadEnvironment(
-        orgName, projName, envName);
+    //const openEnv = await client.openAndReadEnvironment(
+    //    orgName, projName, envName);
+//
+    //if (!openEnv) {
+    //    console.error("Failed to open and read the environment");
+    //    return;
+    //}
+    //const aws_account_id = openEnv.values?.dev.account;
+//
+    //console.log(`Deploying stack for ${pulumi.getStack()}`)
 
-    if (!openEnv) {
-        console.error("Failed to open and read the environment");
-        return;
-    }
-    const aws_account_id = openEnv.values?.dev.account;
-
-    console.log(`Deploying stack for ${pulumi.getStack()}`)
-
-    const builder = new SampleBlogDirector(
-        aws_account_id,
-        orgName,
-        projName)
-    builder.create(Env[pulumi.getStack() as keyof typeof Env]);
+    //const builder = new SampleBlogDirector(
+    //    aws_account_id,
+    //    orgName,
+    //    projName)
+    //builder.create(Env[pulumi.getStack() as keyof typeof Env]);
 }
 
 (async () => {await main()})();
